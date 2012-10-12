76027d51ecabf462d7417fdfb2893c87e70e6c63
Stephen Cannizzaro <cannizzaro75@gmail.com>
2012-10-12 15:10:31 -0400

    getting a random word on refresh now..next up: 24 hr timer, and routes

--- js/views/cursesview.js
+++ js/views/cursesview.js
@@ -11,20 +11,14 @@ $(function() {
 		initialize: function() {
 			this.collection = new PO.Curses(window.curseData);
 			//this.template = _.template( $('#item-template').html() );
-			this.render();
+			//this.render();
 		},
 
 		render: function() {
 			var that = this,
-				randomIndex = _.random(0, this.collection.length),
         		shuffle = _.shuffle(this.collection.models);
-        		
-        		_.each(shuffle, function (item){
-        			that._renderCurse(item);
-        		}, this);
-        	
+        	that._renderCurse(this.collection.at(_.random(0, this.collection.length)));
         	return this;
-        	
     	},
     	
     	_renderCurse: function (item) {
