c6d600b37c9f3562428d51807f4e1548128cf948
Stephen Cannizzaro <cannizzaro75@gmail.com>
2012-10-31 21:28:29 -0400

    just some random cleanup from last time

--- js/backbone/models/curse.js
+++ js/backbone/models/curse.js
@@ -5,5 +5,6 @@ $(function() {
 	
 	//Curse model
 	window.PO.Curse = Backbone.Model.extend({});
+	//to do: add defaults to the model
 	
 }());
--- js/backbone/routers/curseroute.js
+++ js/backbone/routers/curseroute.js
@@ -17,7 +17,12 @@ $(function() {
 		home: function() {
 			var $home = $('#container');
 			$home.empty();
-			$home.append(this.oathView.render().el);			
+			$home.append(this.oathView.render().el);
+			//to do: move view instantiation
+			//and DOM manipulation out of the router
+			//move into view, then call that function
+			//in this home method!
+			//see: http://lostechies.com/derickbailey/2011/12/27/the-responsibilities-of-the-various-pieces-of-backbone-js/		
 		}
 	
 	});
