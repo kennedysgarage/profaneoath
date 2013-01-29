850391b3908a8105662c842445d1e81e15564096
Stephen Cannizzaro <cannizzaro75@gmail.com>
2013-01-28 21:48:01 -0500

    starting to work with the new item suggestion form sub-view, need to eventually style the links in the footer a little better...

--- js/backbone/views/newitemview.js
+++ js/backbone/views/newitemview.js
@@ -1,10 +1,18 @@
 window.PO = window.PO || {};
 $(function() {
 	window.PO.NewItemView = Backbone.View.extend({
-						
+		el: $('footer'),
+		events:{
+			"click #newitemlink":"_suggestForm"
+		},
 		template: _.template($('#item-new').html()),
+		_suggestForm: function (e) {
+			e.preventDefault();
+			this.render().el;
+			console.log('clicking newitemlink works');
+		},
 		render: function() {
-        	$(this.el).html(this.template());
+        	$('#container').html(this.template());
         	return this;
 		}
 	});
